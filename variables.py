97b99572bf33f681afd93a6160c5b49867e45c97
Yogisha Niraula <yogishaniraula19@gmail.com>
2026-09-08 11:39:35 +0545

    variables

---
+++ variables.py
@@ -0,0 +1,25 @@
+#Variables and concatenation
+
+#name = input("Enter your name")
+
+#print("hello" + " " + name)   #concatenation
+
+
+#practice question 1 :
+
+first_name = "Tony"
+second_name = "Shark" 
+age = 52
+height = 1.85 
+isSuperhero = True
+
+question = input("What is his name?")
+
+print("First name :" + first_name)
+print("Secone name :" + second_name)
+print("Age  :" , age)
+print("He is :" , height , "m" + "tall")
+print("Is he a superhero? :" , isSuperhero)
+print("His superhero name is :" + question)
+
+# use {add sign (+) } to join str values, and {comma sigh (,) } to join other values together
