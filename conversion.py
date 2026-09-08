9a0535989a8890c7442c210d2b63abf498314081
Yogisha Niraula <yogishaniraula19@gmail.com>
2026-09-08 11:38:56 +0545

    type

---
+++ conversion.py
@@ -0,0 +1,14 @@
+#type  casting (explicit) : done my programmers
+
+age = input ("Enter your age : ")
+
+new_age = float(age) + 1
+            #bool(), str(), int()
+print(new_age)
+
+
+# Type conversion (implicit): done by python interpreter 
+
+print(1+1.5)
+
+
