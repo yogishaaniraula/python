4033581b1db019bc5939139ee6cebe4860a7ef7f
Yogisha Niraula <yogishaniraula19@gmail.com>
2026-09-09 22:20:07 +0545

    types of operators

---
+++ operators.py
@@ -0,0 +1,25 @@
+#Arithmetic operators
+# print(5-3)
+# print(5%3) # modulo -> remainder
+# print(5**3) #power
+
+
+#Assignment operators
+# x = 2
+# x += 7 # ( x = x+7)
+# print (x) # 9
+
+#Operator precedence ( BODMAS )
+
+# () > *,/ > +, - 
+
+# if * and / on same line : move from left to right
+
+
+
+
+
+
+
+
+
