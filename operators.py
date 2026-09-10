4ac22bf9390dde18039c6f9f12e1b3578e421796
Yogisha Niraula <yogishaniraula19@gmail.com>
2026-09-10 22:58:08 +0545

    operations

--- operators.py
+++ operators.py
@@ -10,11 +10,36 @@
 # print (x) # 9
 
 #Operator precedence ( BODMAS )
-
 # () > *,/ > +, - 
 
 # if * and / on same line : move from left to right
 
+#comparision operators
+# >,<, >=, <=, ==, != 
+# print(2>=8) #false
+# print(8<=10) #true
+# print(2==2) #true
+# print (5!=5) #false
+
+#logical operators
+# or, and, not 
+# st1 = 3>5 #false
+# st2 = 3>2 # true
+
+# print(st1 or st2) # ans = true because for OR, if one is true ans is true
+
+# st1 = 3>1
+# st2 = 3<8
+
+# print (st1 and st2) # ans = true because for AND, if both are true ans is true 
+
+
+# print(not (3>2)) #false 
+
+
+#Conditional statements
+
+age = 24 
 
 
 
