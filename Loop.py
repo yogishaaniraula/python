ca0db983360fb7cac54f2bee36f79b9b0c90051c
Yogisha Niraula <yogishaniraula19@gmail.com>
2026-09-13 14:33:21 +0545

    loops

---
+++ Loop.py
@@ -0,0 +1,53 @@
+#loops
+
+# counter = 1
+# while counter <=5:
+#     print("hi there", counter)
+#     counter += 1
+# print("end of code") 
+
+#output : hi there 1
+# hi there 2
+# hi there 3
+# hi there 4
+# hi there 5
+# end of code
+
+
+i = 1
+while i <=5:
+     print(i * "i") # whenever an integer value is mulitpled by a string, the output the multiple of the string.  
+     i += 1
+print("end of code") 
+
+#output : 
+# i      (1 * i = i)
+# ii     (2 * i = i i) and so on..
+# iii
+# iiii
+# iiiii
+
+
+i = 5 
+while i > 0 :
+    print ( i * "hello ")
+    i -= 1  # i = i -1 = 5 -1 = 4 
+
+#output : 
+# hello hello hello hello hello  ( 5 * hello = hellohellohellohellohello )
+# hello hello hello hello  ( 4 * hello = hellohellohellohello)
+# hello hello hello 
+# hello hello 
+# hello 
+
+#for loops
+
+nums = range(5) 
+
+
+
+
+
+
+
+
