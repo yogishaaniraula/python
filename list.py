10ded51f5b6b45a8a9dd1045dc2da0add1e3b37e
Yogisha Niraula <yogishaniraula19@gmail.com>
2026-09-15 17:19:52 +0545

    list

---
+++ list.py
@@ -0,0 +1,39 @@
+# List
+marks = [45, 98, 44, 85, 23]
+#print(marks)
+# print(marks, type (marks)) 
+
+# Length 
+# print(len(marks))
+
+#index values 
+# print(marks[0])
+# print(marks[-1]) #last value
+# print(marks[-2]) # second last value
+
+#slicing a list : list[starting index :ending index, (no include)]
+
+# print(marks[0:3]) # 45, 98,44
+# print(marks[0:]) # to get all the values
+
+
+#Loop in List
+
+# for score in marks:
+#     print(score)
+
+#List are mutable : values can be changed
+
+# marks.append(45) # add value at the end
+# print(marks) 
+
+# marks.insert(4,38) # adding value at particular index. Template: marks.insert (index value, desired number)
+# print(marks)
+# #output : [45, 98, 44, 85, 38, 23] 
+
+
+#Checking if certain value are in our list or not : 
+
+#print(44 in marks) #output : True 
+
+
