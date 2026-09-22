fe3fb667af6fc73b4c0e2593e672cbf6191895bf
Yogisha Niraula <yogishaniraula19@gmail.com>
2026-09-22 17:56:18 +0545

    funtions

---
+++ function.py
@@ -0,0 +1,38 @@
+# Main logic / function define
+# def cal_gst(price): #parameters
+#     new_price = price + price * 0.18  
+#     print(new_price)
+# #function calling 
+# cal_gst(77) #arguments
+# cal_gst(142)
+
+
+# in-built functions : print, print(len[1,4,2])
+
+# module functions : collection of related fucntion, classes and variables. 
+# 1. import math
+
+# import math 
+# print (dir(math)) #prints out functions related to math
+
+
+# from math import sqrt 
+# print(sqrt(16)) 
+
+# from math import sqrt, log2 
+# print (log2(16))
+
+
+# 2. import random 
+
+import random
+
+# print(random. random()) # generated random number ranging between 0 and 1, but not above 1. 
+# print(random. randint(4, 89)) #generatd random value between different range, which can include 4 and 89 too.
+
+
+
+
+
+
+
